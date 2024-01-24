ba41fc15af2c55073c24f331082bc612db35887f
talikap <talikaplan12@gmail.com>
2024-01-24 12:09:26 +0200

    deploy to railway

--- server/server.js
+++ server/server.js
@@ -18,7 +18,11 @@ app.use((req, res, next) =>{
 })
 // routes
 app.use('/api/codeblocks', codeBlockRoutes)
-
+const port = process.env.PORT || 3000;
+        //listen for requests
+        server.listen(port,"0.0.0.0", () =>{
+            console.log(`connected to db & listening on port ${port}`) 
+        })  
 //connect to db
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
