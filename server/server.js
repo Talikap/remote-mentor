99ae5b73466b4dbadcb8377c42d9f955172c9139
talikap <talikaplan12@gmail.com>
2024-01-24 21:38:46 +0200

    deploy to railway

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const { createServer } = require('node:http')
+const cors = require('cors');
 const codeBlockRoutes = require('./routes/codeblocks')
 const socketHandler = require('./socketHandler')
 
@@ -11,9 +12,13 @@ const app = express()
 
 //middleware
 app.use(express.json())
+app.use(cors());
 
 // routes
 app.use('/api/codeblocks', codeBlockRoutes)
+app.get('/**', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
 
 //connect to db
 mongoose.connect(process.env.MONGO_URI)
