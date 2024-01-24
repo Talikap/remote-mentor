1688fe2c833a582067dbb1793d3ee2731d649726
talikap <talikaplan12@gmail.com>
2024-01-24 16:19:47 +0200

    deploy to railway

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ const server = createServer(app)
 //integrate socket handelling
 socketHandler(server)
 
-app.get('/**', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
