0157c41e9edcb28b99e5c62ea736827876cc78ed
talikap <talikaplan12@gmail.com>
2024-01-25 15:11:04 +0200

    deploy to railway

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ const corsOptions ={
 
 app.use(cors(corsOptions))*/
 app.use(express.json())
-app.use(express.static(path.resolve(__dirname, 'public')))
+
 //app.use(express.urlencoded({ extended: true }))
 app.use('/api/codeblocks', codeBlockRoutes)
 app.get('/*', function(req, res) {
