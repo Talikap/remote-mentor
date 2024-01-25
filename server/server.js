cd060695517394ab50fa637116efd8acfc3cd8ed
talikap <talikaplan12@gmail.com>
2024-01-25 14:21:52 +0200

    deploy to railway

--- server/server.js
+++ server/server.js
@@ -10,14 +10,14 @@ const socketHandler = require('./socketHandler')
 
 
 //express app
-const app = express()
+const app = express()/*
 const corsOptions ={
   origin:'*', 
   credentials:true,            //access-control-allow-credentials:true
   optionSuccessStatus:200,
 }
 
-app.use(cors(corsOptions))
+app.use(cors(corsOptions))*/
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use('/api/codeblocks', codeBlockRoutes)
